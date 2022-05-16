b1616f62fd57333f4cc25a4def5b3643610eb7ef
Brian Pugh <bnp117@gmail.com>
2022-05-15 23:37:10 -0700

    Simple inheritance default example

---
+++ examples/simple_class.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+
+from autoregistry import Registry, abstractmethod
+
+
+@dataclass
+class Pokemon(Registry):
+    level: int
+    hp: int
+
+    @abstractmethod
+    def attack(self, target):
+        """Attack another Pokemon."""
+
+
+class Charmander(Pokemon):
+    def attack(self, target):
+        return 1
+
+
+class Pikachu(Pokemon):
+    def attack(self, target):
+        return 2
+
+
+class SurfingPikachu(Pikachu):
+    def attack(self, target):
+        return 3
+
+
+print("")
+print(f"{len(Pokemon)} Pokemon registered:")
+print(f"    {list(Pokemon.keys())}")
+# By default, lookup is case-insensitive
+charmander = Pokemon["cHaRmAnDer"](level=7, hp=31)
+print(f"Created Pokemon: {charmander}")
+print("")

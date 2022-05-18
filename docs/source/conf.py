0851d922f8605cbbab026479cfc4cc03db8e7bf3
Brian Pugh <bnp117@gmail.com>
2022-05-17 20:37:05 -0700

    fix favicon, remove version and title from readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -77,13 +77,13 @@ html_static_path = ["_static"]
 
 html_title = project
 html_logo = "../../assets/logo_200w.png"
-html_favicohn = "../../assets/favicon.png"
+html_favicon = "../../assets/favicon.png"
 
 html_theme_options = {
     # "analytics_id": "G-XXXXXXXXXX",  # Provided by Google in your dashboard
     # "analytics_anonymize_ip": False,
-    "logo_only": False,
-    "display_version": True,
+    "logo_only": True,
+    "display_version": False,
     "prev_next_buttons_location": "bottom",
     "style_external_links": False,
     "vcs_pageview_mode": "",

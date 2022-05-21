9827a565ee8c56b8bf640743ef8e9a10ea46b83a
Brian Pugh <bnp117@gmail.com>
2022-05-21 07:44:45 -0700

    remove abstractmethod from __init__ and docs

--- autoregistry/__init__.py
+++ autoregistry/__init__.py
@@ -1,8 +1,6 @@
 # Don't manually change, let poetry-dynamic-versioning-plugin handle it.
 __version__ = "0.0.0"
 
-from abc import abstractmethod
-
 from .config import RegistryConfig
 from .exceptions import (
     CannotDeriveNameError,

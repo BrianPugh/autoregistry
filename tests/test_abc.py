673f214e32e84d5963b61ef58171fee978ce2672
Brian Pugh <bnp117@gmail.com>
2022-05-21 07:52:36 -0700

    Add tests explicitly for ABC usage

---
+++ tests/test_abc.py
@@ -0,0 +1,84 @@
+from abc import ABC, abstractmethod
+
+import pytest
+
+from autoregistry import Registry
+
+
+def test_abc_undefined_abstractmethod():
+    class Pokemon(Registry):
+        @abstractmethod
+        def attack(self):
+            pass
+
+    class Charmander(Pokemon):
+        def attack(self):
+            return 1
+
+    class Pikachu(Pokemon):
+        pass
+
+    with pytest.raises(TypeError):
+        Pokemon()
+
+    with pytest.raises(TypeError):
+        Pokemon["pikachu"]()
+
+    with pytest.raises(TypeError):
+        Pikachu()
+
+    Charmander()
+
+
+def test_abc_multiple_inheritence_first():
+    """The programmer doesn't know inheritting from ABC is superfluous."""
+
+    class Pokemon(ABC, Registry):
+        @abstractmethod
+        def attack(self):
+            pass
+
+    class Charmander(Pokemon):
+        def attack(self):
+            return 1
+
+    class Pikachu(Pokemon):
+        pass
+
+    with pytest.raises(TypeError):
+        Pokemon()
+
+    with pytest.raises(TypeError):
+        Pokemon["pikachu"]()
+
+    with pytest.raises(TypeError):
+        Pikachu()
+
+    Charmander()
+
+
+def test_abc_multiple_inheritence_last():
+    """The programmer doesn't know inheritting from ABC is superfluous."""
+
+    class Pokemon(Registry, ABC):
+        @abstractmethod
+        def attack(self):
+            pass
+
+    class Charmander(Pokemon):
+        def attack(self):
+            return 1
+
+    class Pikachu(Pokemon):
+        pass
+
+    with pytest.raises(TypeError):
+        Pokemon()
+
+    with pytest.raises(TypeError):
+        Pokemon["pikachu"]()
+
+    with pytest.raises(TypeError):
+        Pikachu()
+
+    Charmander()

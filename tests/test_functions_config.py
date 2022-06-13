e618326d4731939dde701d97dbd0fb06fe1177f0
Brian Pugh <bnp117@gmail.com>
2022-06-13 13:28:16 +0200

    Dont validate explicitly provided names. Fixed aliases not obeying overwrite config

--- tests/test_functions_config.py
+++ tests/test_functions_config.py
@@ -1,6 +1,7 @@
 import pytest
 
 from autoregistry import Registry
+from autoregistry.exceptions import InvalidNameError, KeyCollisionError
 
 
 def test_decorator_case_sensitive():
@@ -17,7 +18,7 @@ def test_decorator_case_sensitive():
 
 
 def test_decorator_called():
-    registry = Registry(case_sensitive=True)
+    registry = Registry()
 
     @registry()
     def foo():
@@ -26,8 +27,18 @@ def test_decorator_called():
     assert list(registry) == ["foo"]
 
 
+def test_decorator_invalid_name():
+    registry = Registry(suffix="_baz")
+
+    with pytest.raises(InvalidNameError):
+
+        @registry
+        def foo():
+            pass
+
+
 def test_decorator_called_name_override():
-    registry = Registry(case_sensitive=True)
+    registry = Registry()
 
     @registry(name="bar")
     def foo():
@@ -37,8 +48,20 @@ def test_decorator_called_name_override():
     assert registry["bar"] == foo
 
 
+def test_decorator_called_name_override_dont_follow_rules():
+    registry = Registry(suffix="_baz")
+
+    # "bar" doesn't end with "_baz"
+    @registry(name="bar")
+    def foo_baz():
+        pass
+
+    assert list(registry) == ["bar"]
+    assert registry["bar"] == foo_baz
+
+
 def test_decorator_called_aliases_str():
-    registry = Registry(case_sensitive=True)
+    registry = Registry()
 
     @registry(aliases="bar")
     def foo():
@@ -65,7 +88,7 @@ def test_decorator_called_aliases_str_dont_follow_rules():
 
 
 def test_decorator_called_aliases_list():
-    registry = Registry(case_sensitive=True)
+    registry = Registry()
 
     @registry(aliases=["bar", "baz"])
     def foo():
@@ -73,6 +96,35 @@ def test_decorator_called_aliases_list():
 
     assert list(registry) == ["foo", "bar", "baz"]
     assert registry["bar"] == registry["foo"] == registry["baz"] == foo
+
+
+def test_decorator_aliases_collision():
+    registry = Registry()
+
+    @registry
+    def bar():
+        pass
+
+    with pytest.raises(KeyCollisionError):
+
+        @registry(aliases="bar")
+        def foo():
+            pass
+
+
+def test_decorator_aliases_overwrite():
+    registry = Registry(overwrite=True)
+
+    @registry
+    def bar():
+        pass
+
+    @registry(aliases="bar")
+    def foo():
+        pass
+
+    assert list(registry) == ["bar", "foo"]
+    assert registry["bar"] == registry["foo"] == foo
 
 
 def test_module_non_recursive():

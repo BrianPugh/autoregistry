ee74b220c60c2b41a40e81d17237188ab3d08717
Brian Pugh <bnp117@gmail.com>
2023-04-11 19:41:01 -0700

    Added extended registry meta test

---
+++ tests/test_meta_extension.py
@@ -0,0 +1,26 @@
+from autoregistry import RegistryMeta
+
+
+class ExtendedRegistryMeta(RegistryMeta):
+    def __call__(cls, *args, **kwargs):  # noqa: N805
+        out = super().__call__(*args, **kwargs)
+        out.extended_attribute = cls.__name__
+        return out
+
+
+class Foo(metaclass=ExtendedRegistryMeta):
+    pass
+
+
+class Bar(Foo):
+    pass
+
+
+def test_extended_registry():
+    foo = Foo()
+    bar = Bar()
+
+    assert foo.extended_attribute == "Foo"
+    assert bar.extended_attribute == "Bar"
+
+    assert list(Foo) == ["bar"]
